641af7599bcd6925d4142fd51b42d54c6409e96a
remag9330 <remag9330@users.noreply.github.com>
2021-09-29 10:52:49 +1000

    Upgrade to proper AST manipuations
    Old function parsing code was pretty dodgy and failed in a lot of
    places. This makes the parsing code more robust. There's still more work
    to be done (such as tracking local variables in scopes etc) but the main
    thread ticking is back in action (bar some statements that still need
    to be worked on).

--- src/parseCode.tsx
+++ src/parseCode.tsx
@@ -1,63 +1,228 @@
-export type ThreadFunction = (threadId: number, globals: unknown, waitForTick: (threadId: number, locals: Record<string, unknown>, lineNo: number) => Promise<void>) => Promise<void>;
+import { parseScript } from "esprima";
+import { generate } from "escodegen";
+import type * as T from "estree";
+
+export type ThreadFunction = (
+    threadId: number,
+    globals: unknown,
+    waitForTick: (
+        threadId: number,
+        locals: Record<string, unknown>,
+        lineNo: number
+    ) => Promise<void>
+) => Promise<void>;
 
 export function parseCode(code: string): ThreadFunction {
-    const localsInScopes: string[][] = [[]];
-    
-    const lines = ["async (threadId, globals, waitForTick) => {"];
+    const ast = parseScript(code, { loc: true });
 
-    let lineNo = 0;
-    for (let line of code.split("\n")) {
-        const origLine = line;
-
-        if (isStatement(origLine)) {
-            line = `await waitForTick(threadId, ${locals(localsInScopes)}, ${lineNo}); ${line}`;
-        }
-
-        if (isStartBlock(origLine)) {
-            localsInScopes.push([]);
-        }
-
-        if (isEndBlock(origLine)) {
-            localsInScopes.pop();
-        }
-
-        const newLocal = declaredLocal(origLine);
-        if (newLocal) {
-            localsInScopes[localsInScopes.length - 1].push(newLocal);
-        }
-
-        lines.push(line);
-        lineNo++;
+    if (!ensureBodyNodesAreStatements(ast.body)) {
+        throw new Error("Invalid syntax, cannot use directives or import/export statements");
     }
 
-    lines.push("}");
+    ast.body = ast.body.flatMap(visitStmt);
 
-    // var thread: ThreadFunction = () => { throw new Error("Thread function not overridden"); };
-    const fullCode = lines.join("\n");
+    if (!ensureBodyNodesAreStatements(ast.body)) {
+        throw new Error("Invalid syntax, cannot use directives or import/export statements");
+    }
+
+    ast.body = [wrapBodyInMainAsyncFunction(ast.body)];
+
+    const fullCode = generate(ast);
     console.log(fullCode);
     const thread = eval(fullCode) as ThreadFunction;
 
     return thread;
 }
 
-function isStatement(line: string): boolean {
-    return line.trim().length !== 0 && !line.includes("function");
+function ensureBodyNodesAreStatements(nodes: (T.Directive | T.Statement | T.ModuleDeclaration)[]): nodes is T.Statement[] {
+    for (const node of nodes) {
+        if (node.type === "ExpressionStatement" && "directive" in node) {
+            const directive: T.Directive = node;
+            return false;
+        }
+
+        if (node.type === "ImportDeclaration" || node.type === "ExportNamedDeclaration" || node.type === "ExportDefaultDeclaration" || node.type === "ExportAllDeclaration") {
+            const modDec: T.ModuleDeclaration = node;
+            return false;
+        }
+
+        const stmt: T.Statement = node;
+    }
+
+    return true;
 }
 
-function locals(scopes: string[][]): string {
-    const all = scopes.flat();
-    return `{${all.map(l => `"${l}": ${l}`).join(",")}}`;
+function wrapBodyInMainAsyncFunction(stmts: T.Statement[]): T.ExpressionStatement {
+    const funcWrapper: T.ExpressionStatement = {
+        type: "ExpressionStatement",
+        expression: {
+            type: "ArrowFunctionExpression",
+            generator: false,
+            expression: false,
+            async: true,
+            params: [
+                { type: "Identifier", name: "threadId" },
+                { type: "Identifier", name: "globals" },
+                { type: "Identifier", name: "waitForTick" }
+            ],
+            body: {
+                type: "BlockStatement",
+                body: stmts
+            }
+        }
+    }
+
+    return funcWrapper;
 }
 
-function declaredLocal(line: string): string | undefined {
-    const re = /(?:const |let |var )([a-zA-Z0-9]+)/g;
-    return re.exec(line)?.[1];
+function callWaitForTick(lineNo: number): T.CallExpression {
+    return {
+        type: "CallExpression",
+        callee: {
+            type: "Identifier",
+            name: "waitForTick"
+        },
+        arguments: [
+            { type: "Identifier", name: "threadId" },
+            { type: "ObjectExpression", properties: [] },
+            { type: "Literal", value: lineNo, raw: lineNo.toString() }
+        ],
+        optional: false,
+    }
 }
 
-function isStartBlock(line: string): boolean {
-    return line.includes("{");
+function awaitCallWaitForTick(lineNo: number): T.AwaitExpression {
+    return {
+        type: "AwaitExpression",
+        argument: callWaitForTick(lineNo)
+    }
 }
 
-function isEndBlock(line: string): boolean {
-    return line.includes("}");
+function wrapExprInStmt(expr: T.Expression): T.ExpressionStatement {
+    return {
+        type: "ExpressionStatement",
+        expression: expr,
+    };
+}
+
+function wrapExprInAwaitForTickSequenceExpr(expr: T.Expression): T.SequenceExpression {
+    return {
+        type: "SequenceExpression",
+        expressions: [
+            awaitCallWaitForTick(expr.loc!.start.line - 1),
+            expr,
+        ],
+    };
+}
+
+function awaitForTickStmt(lineNo: number): T.ExpressionStatement {
+    return wrapExprInStmt(awaitCallWaitForTick(lineNo));
+}
+
+function prefixStmtWithAwaitForTickStmt(stmt: T.Statement): T.Statement[] {
+    return [awaitForTickStmt(stmt.loc!.start.line - 1), stmt];
+}
+
+function wrapStmtsInBlock(stmts: T.Statement[]): T.BlockStatement {
+    return {
+        type: "BlockStatement",
+        body: stmts,
+    };
+}
+
+function visitStmt(stmt: T.Statement): T.Statement[] {
+    if (stmt.type === "ExpressionStatement") {
+        return prefixStmtWithAwaitForTickStmt(stmt);
+
+    } else if (stmt.type === "BlockStatement") {
+        stmt.body = stmt.body.flatMap(visitStmt);
+        return [stmt];
+
+    } else if (stmt.type === "EmptyStatement") {
+        return prefixStmtWithAwaitForTickStmt(stmt);
+
+    } else if (stmt.type === "DebuggerStatement") {
+        return prefixStmtWithAwaitForTickStmt(stmt);
+
+    } else if (stmt.type === "WithStatement") {
+        stmt.body = wrapStmtsInBlock(visitStmt(stmt.body));
+        return prefixStmtWithAwaitForTickStmt(stmt);
+
+    } else if (stmt.type === "ReturnStatement") {
+        return prefixStmtWithAwaitForTickStmt(stmt);
+
+    } else if (stmt.type === "LabeledStatement") {
+        throw new Error("Not implemented");
+        
+    } else if (stmt.type === "BreakStatement") {
+        return prefixStmtWithAwaitForTickStmt(stmt);
+
+    } else if (stmt.type === "ContinueStatement") {
+        return prefixStmtWithAwaitForTickStmt(stmt);
+
+    } else if (stmt.type === "IfStatement") {
+        stmt.consequent = wrapStmtsInBlock(visitStmt(stmt.consequent));
+        stmt.alternate = stmt.alternate ? wrapStmtsInBlock(visitStmt(stmt.alternate)) : stmt.alternate;
+        return prefixStmtWithAwaitForTickStmt(stmt);
+
+    } else if (stmt.type === "SwitchStatement") {
+        throw new Error("Not implemented");
+        
+    } else if (stmt.type === "ThrowStatement") {
+        return prefixStmtWithAwaitForTickStmt(stmt);
+
+    } else if (stmt.type === "TryStatement") {
+        stmt.block.body = stmt.block.body.flatMap(visitStmt);
+        if (stmt.handler) {
+            stmt.handler.body.body = stmt.handler.body.body.flatMap(visitStmt);
+        }
+        if (stmt.finalizer) {
+            stmt.finalizer.body = stmt.finalizer.body.flatMap(visitStmt);
+        }
+        return prefixStmtWithAwaitForTickStmt(stmt);
+
+    } else if (stmt.type === "WhileStatement") {
+        stmt.test = wrapExprInAwaitForTickSequenceExpr(stmt.test);
+        stmt.body = wrapStmtsInBlock(visitStmt(stmt.body));
+        return [stmt];
+        
+    } else if (stmt.type === "DoWhileStatement") {
+        stmt.test = wrapExprInAwaitForTickSequenceExpr(stmt.test);
+        stmt.body = wrapStmtsInBlock(visitStmt(stmt.body));
+        return [stmt];
+        
+    } else if (stmt.type === "ForStatement") {
+        if (stmt.test) {
+            stmt.test = wrapExprInAwaitForTickSequenceExpr(stmt.test);
+        }
+        if (stmt.update) {
+            stmt.update = wrapExprInAwaitForTickSequenceExpr(stmt.update);
+        }
+        stmt.body = wrapStmtsInBlock(visitStmt(stmt.body));
+
+        return prefixStmtWithAwaitForTickStmt(stmt);
+        
+    } else if (stmt.type === "ForInStatement") {
+        stmt.body = wrapStmtsInBlock(visitStmt(stmt.body));
+        return prefixStmtWithAwaitForTickStmt(stmt);
+        
+    } else if (stmt.type === "ForOfStatement") {
+        stmt.body = wrapStmtsInBlock(visitStmt(stmt.body));
+        return prefixStmtWithAwaitForTickStmt(stmt);
+        
+    } else if (stmt.type === "FunctionDeclaration") {
+        stmt.async = true;
+        stmt.body.body = stmt.body.body.flatMap(visitStmt);
+        return [stmt];
+        
+    } else if (stmt.type === "VariableDeclaration") {
+        return prefixStmtWithAwaitForTickStmt(stmt);
+        
+    } else if (stmt.type === "ClassDeclaration") {
+        throw new Error("Not implemented");
+        
+    } else {
+        const exhaustive: never = stmt;
+        throw new Error("Exhaustive check failed: " + JSON.stringify(exhaustive));
+    }
 }
